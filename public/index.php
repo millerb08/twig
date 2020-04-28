7bf2f0b11d672cec97b248ac06539c714e738722
millerb08 <miller.steffens@gmail.com>
2020-04-28 17:06:23 -0500

    fixing navbar

--- public/index.php
+++ public/index.php
@@ -4,4 +4,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../templates');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('home.twig', ['name' => 'miller']);
+
+$nav = [
+    "home" =>[
+        "href" => "/",
+        "caption" => "Welcome",
+        "status" => "active"
+    ],
+    "contact" =>[
+        "href" => "/contact",
+        "caption" => "Contact Us",
+        "status" => false
+    ]
+];
+echo $twig->render('home.twig', ['name' => 'miller', "nav" => $nav]);
